43b1a0dbf7b21d72c65c0aa5f840b3d5f013c62e
Glitch (glitch-hello-react) <none>
2022-03-25 02:11:22 +0000

    🐄💓 Checkpoint ./src/pages/home.jsx:36021536/254

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export default function Home() {
   return (
     <>
-      <h1>Ben N' Bros Grass Co.</h1>
+      <h2>Ben N' Bros Grass Co.</h2>
       <body>
         <p>
           <h3><b>Services Offered:</b></h3>
@@ -15,6 +15,14 @@ export default function Home() {
         <br></br>
         <p>
           <h3><b>The Team:</b></h3>
+          <hr></hr>
+          Ben Adams a.k.a <em>the boss</em><br></br>
+          Charlie Adams<br></br>
+          Beau Adams<br></br>
+        </p>
+        <br></br>
+        <p>
+          <h3><b>Contact Us</b></h3>
           <hr></hr>
           Ben Adams a.k.a <em>the boss</em><br></br>
           Charlie Adams<br></br>
