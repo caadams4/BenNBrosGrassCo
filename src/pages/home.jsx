c256e246d54367ca81c3cc7571d1d58a843694f6
Glitch (glitch-hello-react) <none>
2022-03-25 01:35:32 +0000

    🏛🔋 Checkpoint ./src/pages/home.jsx:36021536/763

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,23 @@
 import * as React from "react";
 
 export default function Home() {
-  
   return (
     <>
       <h1>Ben N' Bros Grass Co.</h1>
-      <div><b>Services Offered:</b></div>
-      <p>
-        Grass Cutting<br></br>
-        Weed Wacking<br></br>
-        Yard Cleanup<br></br>
-      </p>
+      <body>
+        <div>
+          <b>Services Offered:</b>
+        </div>
+        <p>
+          Grass Cutting<br></br>
+          Weed Wacking<br></br>
+          Yard Cleanup<br></br>
+        </p>
+      </body>
+      <footer>
+        
+      
+      </footer>
     </>
   );
 }
