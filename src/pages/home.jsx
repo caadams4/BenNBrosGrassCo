784619e059aa40556e40d9630b9cf7efc74423e9
Glitch (glitch-hello-react) <none>
2022-03-25 02:34:23 +0000

    🍁🎣 Checkpoint ./src/pages/home.jsx:36021536/130

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,24 +6,22 @@ export default function Home() {
       <h2>Ben N' Bros Grass Co.</h2>
       <body>
         <p>
-          <h3><b>Services Offered:</b></h3>
-          <hr></hr>
+          <h3><b>Services Offered:</b><hr></hr></h3>
           Grass Cutting<br></br>
           Weed Wacking<br></br>
           Yard Cleanup<br></br>
+          Dog walking<br></br>
         </p>
         <br></br>
         <p>
-          <h3><b>The Team:</b></h3>
-          <hr></hr>
+          <h3><b>The Team:</b><hr></hr></h3>
           Ben Adams a.k.a <em>the boss</em><br></br>
           Charlie Adams<br></br>
           Beau Adams<br></br>
         </p>
         <br></br>
         <p>
-          <h3><b>Contact Us</b></h3>
-          <hr></hr>
+          <h3><b>Contact Us</b><hr></hr></h3>
           Email:<br></br>
           Phone:
           
