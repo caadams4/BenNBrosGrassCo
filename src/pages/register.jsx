6986424a8e9452c690e07a35302002471fa129c0
Glitch (glitch-hello-react) <none>
2022-03-28 03:24:38 +0000

    📡🐕 Checkpoint ./src/pages/register.jsx:36021536/2034 ./src/pages/home.jsx:36021536/42

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,27 +1,36 @@
 import * as React from "react";
-import {Navbar,Button,Col,} from 'reactstrap';
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
+import { Navbar, Button, Container, Row ,Col  } from "reactstrap";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 export default function Register() {
   return (
     <div>
-        
       <Calendar></Calendar>
       <p>
-        Avalible Times: #DROPDOWN#<br></br>
-        Services: #RADIO BUTTON#<br></br>
-        Phone Numeber: #TEXTBOX#<br></br>
-        Email: #TEXTBOX#<br></br>
-        Address: #TEXTBOX#<br></br>
-        
-      
+        <Container>
+          <Row>
+            <Col xs="6">
+              Avalible Times: <br></br>
+              Services:<br></br>
+              Phone Numeber: <br></br>
+              Email: <br></br>
+              Address: <br></br>
+            </Col>
+
+            <Col className='sm'>
+              #DROPDOWN#<br></br>
+              #RADIO BUTTON#<br></br>
+              #TEXTBOX#<br></br>
+              #TEXTBOX#<br></br>
+              #TEXTBOX#<br></br>
+            </Col>
+          </Row>
+        </Container>
       </p>
-      
-      <Button>fffff</Button>
+
     </div>
-        
-    )
+  );
 }
 
 //
