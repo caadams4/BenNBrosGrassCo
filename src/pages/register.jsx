2233210b9782d0972c64f90482c14da50542286f
Glitch (glitch-hello-react) <none>
2022-03-31 12:35:42 +0000

    🌈🏠 Checkpoint ./src/pages/register.jsx:36021536/3799 ./package.json:36021536/1734

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,13 +1,27 @@
 import * as React from "react";
-import { Container, Row, Col, Dropdown} from "react-bootstrap";
+//import { Container, Row, Col} from "react-bootstrap";
+import Dropdown from 'react-bootstrap/Dropdown';
 import Calendar from "react-calendar";
+import 'bootstrap/dist/css/bootstrap.css';
 import "react-calendar/dist/Calendar.css";
 
 export default function Register() {
   return (
     <div>
       <Calendar></Calendar>
-      <form></form>
+      <form>
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-basic">
+            Choose your time
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">9 </Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </form>
     </div>
   );
 }
